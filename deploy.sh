f1c9c5036ce4e90e2bfd7d135cdfc88542e782fb
CEDRIC DERUE <cedric.derue@altran.com>
2017-07-08 18:46:28 +0200

    add deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,1 @@
-#!/usr/bin/env bash
 curl -H "Content-Type: application/json" --data '{"build": true}' -X POST https://registry.hub.docker.com/u/cderue/azurecamp-nodejs-webapp/trigger/1d191222-6252-4b3a-b734-da5be2c4cb3e/
